d35ef4f07dc4dc0aa7101c966d6dbf59ab1ac5fe
Konstantin Pugachev <kpugachev@infinet.ru>
2016-08-26 13:32:33 +0500

    Migrato to rapidoid, x10 performance

--- src/main/java/com/github/the20login/trivial/bankng/Main.java
+++ src/main/java/com/github/the20login/trivial/bankng/Main.java
@@ -1,28 +1,55 @@
 package com.github.the20login.trivial.bankng;
 
-import com.github.the20login.trivial.bankng.keylock.KeylockProcessing;
 import com.github.the20login.trivial.bankng.stm.StmProcessing;
-import fi.iki.elonen.NanoHTTPD;
+
+import org.rapidoid.lambda.OneParamLambda;
+import org.rapidoid.lambda.ThreeParamLambda;
+import org.rapidoid.setup.On;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
 
-public class Main extends NanoHTTPD {
-    private static final Response NOT_FOUND = newFixedLengthResponse(Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "Not found");
+public class Main {
     private final Processing processing;
 
     public Main() throws IOException {
-        super(8080);
         Map<Long, Long> map = new HashMap<>();
         for (long i = 0; i < 10000; i++){
             map.put(i, 1_000_000_000L);
         }
         processing =  new StmProcessing(map);
 
-        this.setAsyncRunner(new BoundRunner(Executors.newFixedThreadPool(4)));
-        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        On.address("0.0.0.0").port(8080);
+
+        On.get("/transfer/{from}/{to}/{amount}").plain(new ThreeParamLambda<String, Long, Long, Long>() {
+            @Override
+            public String execute(Long from, Long to, Long amount) throws Exception {
+                if (processing.transfer(from, to , amount))
+                    return "Transferred " + amount + " money from " + from + " to " + to;
+                return "Not transferred";
+            }
+        });
+
+        On.get("/bankBalance").plain(processing::totalBalance);
+        On.get("/balance").plain(()->{
+            StringBuilder builder = new StringBuilder();
+            processing.snapshot().entrySet().stream()
+                .forEach(entry-> {
+                    builder.append(entry.getKey());
+                    builder.append("\t");
+                    builder.append(entry.getValue());
+                    builder.append("\n");
+                });
+            return builder.toString();
+        });
+        On.get("/balance/{account}").plain(new OneParamLambda<String, Long>() {
+            @Override
+            public String execute(Long account) throws Exception {
+                return "Account balance " + processing.accountBalance(account);
+            }
+        });
+
         System.out.println("\nRunning! Point your browers to http://localhost:8080/ \n");
     }
 
@@ -31,70 +58,6 @@ public class Main extends NanoHTTPD {
             new Main();
         } catch (IOException ioe) {
             System.err.println("Couldn't start server:\n" + ioe);
-        }
-    }
-
-    @Override
-    public Response serve(IHTTPSession session) {
-        String[] path = session.getUri().split("/");
-        if (path.length < 2)
-            return NOT_FOUND;
-        switch (path[1]) {
-            case "transfer":
-            {
-                if (path.length != 5)
-                    return NOT_FOUND;
-                long from, to, amount;
-                try {
-                    from = Long.valueOf(path[2]);
-                    to = Long.valueOf(path[3]);
-                    amount = Long.valueOf(path[4]);
-                }
-                catch (NumberFormatException e)
-                {
-                    return newFixedLengthResponse(Response.Status.BAD_REQUEST, NanoHTTPD.MIME_PLAINTEXT, "Can't parse");
-                }
-
-                if (processing.transfer(from, to, amount))
-                    return newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "Transferred " + amount + " money from " + from + " to " + to);
-                else
-                    return newFixedLengthResponse(Response.Status.BAD_REQUEST, NanoHTTPD.MIME_PLAINTEXT, "Not transferred");
-            }
-            case "bankBalance": {
-                if (path.length == 2)
-                    return newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "Total balance " + processing.totalBalance());
-                else
-                    return NOT_FOUND;
-            }
-            case "balance":
-            {
-                if (path.length == 2) {
-                    StringBuilder builder = new StringBuilder();
-                    processing.snapshot().entrySet().stream()
-                            .forEach(entry-> {
-                                builder.append(entry.getKey());
-                                builder.append("\t");
-                                builder.append(entry.getValue());
-                                builder.append("\n");
-                            });
-                    return newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, builder.toString());
-                }
-                else if (path.length == 3){
-                    long account;
-                    try {
-                        account = Long.valueOf(path[2]);
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        return newFixedLengthResponse(Response.Status.BAD_REQUEST, NanoHTTPD.MIME_PLAINTEXT, "Can't parse");
-                    }
-                    return newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "Total balance " + processing.accountBalance(account));
-                }
-                else
-                    return NOT_FOUND;
-            }
-            default:
-                return NOT_FOUND;
         }
     }
 }
